301914343b4ea353dae50c526d4393b01eac0f4f
Mukul <kmukul1197@gmail.com>
2019-11-27 16:40:52 +0530

    create structure to take user input

--- students.c
+++ students.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
+struct student{
+    char str[20];
+    int roll;
+    int class;
+    char sec;
+}std[];
+void add(int x){
+    for(int i=0; i<x; i++){
+    printf("Enter the name,class, section and roll no. respectively\n");
+    scanf("%s",std[x].str);
+    scanf("%d",&std[x].class);
+    scanf("%c ",&std[x].sec);
+    scanf("%d",&std[x].roll);
+    
+    }
+}
 void listallstudent(void){
 
 }
 void searchroll(void){
 
 }
-void searchname(){
+void searchname(void){
 
 }
-void addstudent(){
-    
+void addstudent(void){
+    int s;
+    printf("Enter the number of students\n");
+    scanf("%d",&s);
+    add(s);
 }
 int main(){
-    int n;
+    int n,s;
      printf("This programme can perform following operations. Choose your operation and the respective number.\n");
         printf("1. Show list of all students.\n2. Find student by roll number.\n3. Find student by name.\n4. Add a new student.\n5. Quit the programme.\n");
     while(1>0){
