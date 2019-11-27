48d1f90f0195c9c06c1c38fba23f54fa889cc032
Mukul <kmukul1197@gmail.com>
2019-11-27 15:56:54 +0530

    give some instruction about programme

--- students.c
+++ students.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
 int main(){
-    printf("Yes it's working!");
+    int n;
+     printf("This programme can perform following operations. Choose your operation and the respective number.\n");
+        printf("1. Show list of all students.\n2. Find student by roll number.\n3. Find student by name.\n4. Add a new student.\n5. Quit the programme.\n");
+    while(1>0){
+       
+    }
 }
