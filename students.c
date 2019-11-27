7a7df53f19711c646f5b4c224c4f1c4d2bf9aa03
Mukul <kmukul1197@gmail.com>
2019-11-27 16:13:28 +0530

    Basic structure

--- students.c
+++ students.c
@@ -4,6 +4,24 @@ int main(){
      printf("This programme can perform following operations. Choose your operation and the respective number.\n");
         printf("1. Show list of all students.\n2. Find student by roll number.\n3. Find student by name.\n4. Add a new student.\n5. Quit the programme.\n");
     while(1>0){
-       
+       printf("Enter the number according your operation\n");
+       scanf("%d",&n);
+       switch (n)
+       {
+        case 1:
+           listallstudent();
+            break;
+        case 2:
+            searchroll();
+            break;
+        case 3:
+           searchname();
+            break;
+        case 4:
+           addstudent();
+            break;
+        default:
+        printf("Try again\n");
+       }
     }
 }
